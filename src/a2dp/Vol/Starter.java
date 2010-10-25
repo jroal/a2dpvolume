c79edc85b37b6b6d10543e244453ed0dd8353c76
jimroal <jimroal@gmail.com>
2010-10-25 05:49:23 -0500

    Fixed orientation problem.  Now supports landscape as well as portrait.

--- src/a2dp/Vol/Starter.java
+++ src/a2dp/Vol/Starter.java
@@ -4,21 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 
 public class Starter extends BroadcastReceiver{
  	SharedPreferences preferences;   
- 	private MyApplication application;
+ 	
  	public static final String PREFS_NAME = "a2dp.Vol_preferences";
  	
 	@Override
 	public void onReceive(Context context, Intent arg1) {
 		
-		//this.application = (MyApplication) this.;
-		///data/data/a2dp.Vol/shared_prefs/a2dp.Vol_preferences.xml
 		preferences = context.getSharedPreferences(PREFS_NAME,0);
-		//preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
 		if(preferences.getBoolean("bootstart", false) ){
 				context.startService(new Intent(context, service.class));
 		}
