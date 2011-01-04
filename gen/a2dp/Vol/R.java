da7455cf288cb34d1280813a6a64f81c4312211f
jimroal <jimroal@gmail.com>
2011-01-03 21:02:56 -0600

    Added a configuration (preference) checkbox to enable/disable passive mode locations.  This was done since passive locations can sometimes be unreliable.  Also changed launch mode of the activities to singleInstance.  This prevents multiple instances from running.

--- gen/a2dp/Vol/R.java
+++ gen/a2dp/Vol/R.java
@@ -112,6 +112,9 @@ public final class R {
         public static final int Yes=0x7f050027;
         public static final int app_name=0x7f050000;
         public static final int btEnableFail=0x7f050030;
+        public static final int usePassiveOff=0x7f050031;
+        public static final int usePassiveOn=0x7f050032;
+        public static final int usePassiveTitle=0x7f050033;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
