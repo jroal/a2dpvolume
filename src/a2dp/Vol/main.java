355c68bbc7709586aad7b5c3fe59d2638c273cff
jimroal <jimroal@gmail.com>
2012-11-21 08:33:09 -0600

    Fixed issue 142

--- src/a2dp/Vol/main.java
+++ src/a2dp/Vol/main.java
@@ -719,7 +719,7 @@ public class main extends Activity {
 							btDevice bt = new btDevice();
 							i++;
 							String name;
-							if (android.os.Build.VERSION.SDK_INT >= 14){
+							if (android.os.Build.VERSION.SDK_INT >= 14 && android.os.Build.VERSION.SDK_INT <= 16){
 								try {
 									name = ibta.getRemoteAlias(device.getAddress());
 									//Toast.makeText(application, "try made it" + name, Toast.LENGTH_LONG).show();
